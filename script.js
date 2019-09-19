e01698f71215ffc9fff29cd94c8b0c49ac5ed9da
mohammad-khamaiseh-2 <m.khamaiseh2@student.aaup.edu>
2019-09-19 11:25:01 +0300

    Question Page Update js

--- script.js
+++ script.js
@@ -27,87 +27,87 @@ $(function() {
   var questions = [{
           question: "JavaScript is ______ language.",
           options: ['Scripting', 'Programming', 'Both a and b', 'Application'],
-          answer: "a"
+          answer: "1"
       },
       {
           question: "JavaScript is ______ Side scripting language..",
           options: ['Server', 'Client', 'ISP', 'Browser'],
-          answer: "d"
+          answer: "4"
       },
       {
           question: " ______ tag is an extension to HTML that can enclose any number of JavaScript statements.",
           options: ['SCRIPT', ' BODY', 'HEAD', 'TITLE'],
-          answer: "A"
+          answer: "1"
       },
       {
           question: "The _______ method of an Array object adds and/or removes elements from an array.",
           options: ['Reverse', 'Shift', 'Slice', 'Splice'],
-          answer: "D"
+          answer: "4"
       },
       {
           question: "Which of the following is not a valid JavaScript variable name?",
           options: ['2names', '_first_and_last_names', 'FirstAndLast', 'None'],
-          answer: "A"
+          answer: "1"
       },
       {
           question: "Which of the following function of String object creates a string to be displayed as bold as if it were in a <b> tag?",
           options: ['anchor()', 'big()', 'blink()', 'bold()'],
-          answer: "d"
+          answer: "4"
       },
       {
           question: "Which of the following function of Array object returns true if every element in this array satisfies the provided testing function?",
           options: ['concat()', 'every()', 'push()', 'some()'],
-          answer: "b"
+          answer: "2"
       },
       {
           question: "Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?",
           options: ['unshift()', 'sort()', 'splice()', 'toString()'],
-          answer: "A"
+          answer: "1"
       },
       {
           question: "HTML stands for?",
           options: ['Hyper Text Markup Language', 'High Text Markup Language', 'Hyper Tabular Markup Language', 'None of these'],
-          answer: "a"
+          answer: "1"
       },
       {
           question: "Web pages starts with which ofthe following tag?",
           options: ['Body', 'Title', 'HTML', 'Form'],
-          answer: "c"
+          answer: "3"
       },
       {
           question: "Choose the correct HTML tag for the largest heading.",
           options: ['H1', 'Heading', 'Head', 'H6'],
-          answer: "a"
+          answer: "1"
       },
       {
           question: "What does CSS stand for?",
           options: ['Creative Style Sheets', 'Colorful Style Sheets', 'Cascading Style Sheets', 'Computer Style Sheets'],
-          answer: "c"
+          answer: "3"
       },
       {
           question: "Which HTML attribute is used to define inline styles?",
           options: ['font', 'class', 'styles', 'style'],
-          answer: "d"
+          answer: "4"
       },
       {
           question: "Which CSS property controls the text size?",
           options: ['font-size', 'font-style', 'text-style', 'Application'],
-          answer: "a"
+          answer: "1"
       },
       {
           question: "If we want define style for an unique element, then which css selector will we use ?",
           options: ['Id', 'text', 'class', 'name'],
-          answer: "a"
+          answer: "1"
       },
       {
           question: "If we want to use a nice looking green dotted border around an image, which css property will we use?",
           options: ['border-color', 'border-decoration', 'border-style', 'border-line'],
-          answer: "c"
+          answer: "3"
       },
       {
           question: "The default value of 'position' attribute is _________.",
           options: ['fixed', 'absolute', 'inherit', 'relative'],
-          answer: "d"
+          answer: "4"
       },
   ]
 
@@ -130,12 +130,21 @@ $(function() {
 
           showQuestion();
 
-          var timerId = setInterval(function() {
+          var timerId = setInterval(showQuestion, 7000);
+
+          function showQuestion() {
               if (count >= 5) {
                   clearInterval(timerId);
                   $('#questionPage').hide();
                   $('#resultPage').show();
               }
+              $(document).keypress(function(e) {
+                  var code = e.keyCode;
+                  console.log(code + ' inside keypress')
+                  chechAnwser(code);
+
+              });
+
               var randomNumber = getRandom();
               $('#question h4').text(questions[randomNumber].question);
               questions[randomNumber].options.forEach((element, i) => {
@@ -145,10 +154,35 @@ $(function() {
               questions.splice(randomNumber, 1);
               count++;
               // console.log(count);
-          }, 1000);
+          }
 
-      }
-  })
+          function chechAnwser(keyCode, answer) {
+              console.log('inside function')
+              var one = 97;
+              var two = 115;
+              var three = 100;
+              var four = 102;
+              var five = 107;
+              var six = 108;
+              var seven = 59;
+              var eight = 39;
+
+              var player1Answers = [one, two, three, four];
+              var player2Answers = [five, six, seven, eight];
+
+              if (player1Answers.includes(keyCode)) {
+                  console.log('right key for player 1')
+              }
+              if (player2Answers.includes(keyCode)) {
+                  console.log('right key for player 2')
+              }
+          }
+
+
+
+      } // end of else 
+
+  }) // end of click button
 
 
 
@@ -156,23 +190,4 @@ $(function() {
       return Math.floor(Math.random() * Math.floor(questions.length - 1));
   }
 
-  function showQuestion() {
-      var randomNumber = getRandom();
-      $('#question h4').text(questions[randomNumber].question);
-      questions[randomNumber].options.forEach((element, i) => {
-          $(`#answer${i+1}`).html(i + 1 + '- ' + element);
-      });
-
-      questions.splice(randomNumber, 1);
-      count++;
-      console.log(count);
-  }
 });
-
-
-
-// $(document).keypress(function(e){
-
-//   alert(e.keyCode + '  ' + typeof(e.keyCode))
-
-// });
